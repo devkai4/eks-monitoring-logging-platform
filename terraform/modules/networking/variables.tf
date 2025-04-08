ecdc515ab203dbd7d348025d468f663b28e72924
devkai4 <devkai4@pm.me>
2025-04-08 20:57:11 +0900

    feat(terraform): Define networking module variables
    - Add VPC and subnet configuration variables
    - Configure region and availability zone settings for ap-northeast-1
    - Add NAT gateway control variables with sensible defaults
    - Include environment variable and resource tagging support

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,52 @@
+variable "environment" {
+  description = "Environment (dev, stage, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-northeast-1a", "ap-northeast-1c"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Should be true if you want to provision NAT Gateways"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single NAT Gateway for all private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
